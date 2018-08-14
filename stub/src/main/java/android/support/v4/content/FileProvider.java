f05f23411549586d2d60832c722ff4810b051248
Ringo-Freek <hitting@yandex.ru>
2018-08-14 11:06:18 +0700

    Зависимости от библиотек

---
+++ stub/src/main/java/android/support/v4/content/FileProvider.java
@@ -0,0 +1,45 @@
+package android.support.v4.content;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import java.io.File;
+
+public class FileProvider extends ContentProvider {
+    public static Uri getUriForFile(Context context, String param, File file) {
+        throw new RuntimeException("stub!");
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
---
+++ stub/src/main/java/com/gun0912/tedpermission/PermissionListener.java
@@ -0,0 +1,9 @@
+package com.gun0912.tedpermission;
+
+import java.util.ArrayList;
+
+public interface PermissionListener {
+    void onPermissionGranted();
+
+    void onPermissionDenied(ArrayList<String> deniedPermissions);
+}
